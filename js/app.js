20cf674bc40c779e68fc31efafeebbe01fa0c69d
bolshchikov <sergey@bolshchikov.net>
2014-03-16 18:09:19 +0200

    checkbox implementation works

--- js/app.js
+++ js/app.js
@@ -6,14 +6,38 @@
 (function () {
   window.App = Ember.Application.create({
     cities: ['Moscow', 'New York', 'Toronto', 'Paris', 'Berlin', 'Tel Aviv'],
-    selected: []
+    selected: ['Moscow', 'New York']
   });
 
   App.MultiSelectionComponent = Ember.Component.extend({
     classNames: ['multi-selection-container'],
     SelectionContainer: Ember.View.extend({
-      classNames: ['selection-container']
+      classNames: ['selection-container'],
+      valuesRepresentation: function () {
+        return this.get('parentView.values').join(', ');
+      }.property('parentView.values.@each')
     }),
-    DropdownMenu: Ember.View.extend({})
+    DropdownMenu: Ember.CollectionView.extend({
+      tagName: 'ul',
+      classNames: ['dropdown-menu'],
+      contentBinding: 'parentView.content',
+      valuesBinding: 'parentView.values',
+      itemViewClass: Ember.View.extend({
+        templateName: 'dropdown-item',
+        isSelected: function () {
+          return this.get('parentView.values').contains(this.content);
+        }.property('parentView.values.@each'),
+        click: function () {
+          if (!this.get('parentView.values').contains(this.content)) {
+            this.get('parentView.values').pushObject(this.content);
+            this.set('isSelected', true);
+          }
+          else {
+            this.get('parentView.values').removeObject(this.content);
+            this.set('isSelected', false);
+          }
+        }
+      })
+    })
   });
 })();
